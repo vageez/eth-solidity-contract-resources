6ce91c1a63b2fd95c6e5fcf5225b33f299c787c8
Angelo Vagenas <37623247+vageez@users.noreply.github.com>
2021-09-17 21:24:14 -0400

    Create Visibility.sol

---
+++ Visibility.sol
@@ -0,0 +1,44 @@
+pragma solidity 0.7.5;
+
+/**
+ * Visibility
+ * 
+ * Public: Anyone can execute the function, or access our variable.
+ * 
+ * Private: Who can execute or access the fcuntion; Can only be executed from within the contract itself
+ * 
+ * Internal: Same as private, also accessible from contracts deriving from it, that inherit it. 
+ * 
+ * External: Only accessibile from outside contracts, used in automated interactions with contracts
+ * 
+ **/
+ 
+ 
+contract Visibility {
+    
+    mapping(address => uint) balance;
+    
+    function addBalance(uint _toAdd) public returns (uint) {
+        balance[msg.sender] += _toAdd;
+        return balance[msg.sender];
+    }
+    
+    function getBalance() public view returns (uint) {
+        
+        return balance[msg.sender];
+    }
+    
+    function transfer(address recipient, uint amount) public {
+        // check balance of msg.sender
+        
+        _transfer(msg.sender, recipient, amount);
+
+        // event logs and further checks
+    }
+     
+    function _transfer(address from, address to, uint amount) private {
+        balance[from] -= amount;
+        balance[to] += amount;         
+    }
+     
+}
