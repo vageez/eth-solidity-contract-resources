56e69096c0147ceee7795a774bb90ba6ebd3761a
Angelo Vagenas <37623247+vageez@users.noreply.github.com>
2021-09-25 16:20:41 -0400

    Update MultiSig.sol

--- MultiSig.sol
+++ MultiSig.sol
@@ -1,91 +1,83 @@
 pragma solidity 0.7.5;
 pragma abicoder v2;
 
-/**
- * Here
- * 
- **/
- 
- 
-contract MultiSug {
-    event depositAdded (uint amount, address depositor);
-    event transferRequestSubmitted (uint amount, address requestAddr);
-    event transferRequestApproval (uint txId, address requestAddr, bool abool);
-    uint public num_signers;
-    address[] public addresses;
-    mapping(address => mapping(uint => bool)) approvals;    
+
+contract Wallet{ 
     
-    struct TransferRequest {
-        address requestAddr;
-        uint amount;
-        uint approvals;
+    address[] public owners;
+    uint public limit;
+    
+    struct Transfer {
         uint txId;
+        uint amount;
+        address payable receiver;
+        uint approvals;
+        bool hasBeenSent;
     }
     
-    TransferRequest[] transferRequests;
-
-
-    constructor(address[] memory _address, uint _num_signers) {
-        num_signers = _num_signers;
+    Transfer[] transferRequests;
+    mapping(address => mapping(uint => bool)) approvals;
+    
+    modifier ownlyOwners() {
+        bool isOwner;
+        for(uint i = 0; i < owners.length; i++){
+            if(owners[i] == msg.sender){
+                isOwner = true;
+            }
+        }
+        require(isOwner == true);
+        _;
+    }
+    
+    modifier selfApprovalRestriction (uint _txId) {
+        require(transferRequests[_txId].receiver != msg.sender);
+        _;
+    }
+    
+    constructor(address[] memory _owners, uint _limit){
+        limit = _limit;
         
-        for(uint i = 0; i < _address.length; i++){
-            addresses.push(_address[i]);
+        for(uint i = 0; i < _owners.length; i++){
+            owners.push(_owners[i]);
         }
     }
     
-    function deposit() public payable {
-        emit depositAdded(msg.value, msg.sender);
-    }
-    
-    function getBalance() public view returns(uint) {
+    function getBalance() public view ownlyOwners returns (uint) {
         return address(this).balance;
     }
     
-    function setTransferRequest(uint _amount) public {
-     bool addressIsSigner = false;
-     for(uint i = 0; i < addresses.length; i++){
-         if(addresses[i] == msg.sender){
-             addressIsSigner = true;
-         }
-     }
-     if(addressIsSigner) {
-        TransferRequest memory transferRequest = TransferRequest({
-             requestAddr: msg.sender, 
-             amount: _amount, 
-             approvals: 0, 
-             txId: transferRequests.length
-         });
-         
-         transferRequests.push(transferRequest);
-         emit transferRequestSubmitted(_amount, msg.sender);
-     } else {
-        revert();
-     }
-     
-    }
-    
-    function approveTransferRequest(uint _txId) public {
-        require(transferRequests[_txId].requestAddr != msg.sender);
-         transferRequests[_txId].approvals = transferRequests[_txId].approvals + 1;
-         approvals[msg.sender][_txId] = true;
-         emit transferRequestApproval(_txId, msg.sender, approvals[msg.sender][_txId]);
-    }
-    
-    function isTrasferable(uint _txId) private {
+    function deposit() public payable {
         
     }
     
-    
-    function getTransferRequest(uint _txId) public view returns(address, uint, uint, uint) {
-        return (
-            transferRequests[_txId].requestAddr, 
-            transferRequests[_txId].amount, 
-            transferRequests[_txId].approvals, 
-            transferRequests[_txId].txId
-        );
+    function createTransfer(uint _amount, address payable _receiver) public ownlyOwners {
+        Transfer memory transferRequest = Transfer({
+            txId: transferRequests.length,
+            amount: _amount,
+            receiver: _receiver,
+            hasBeenSent: false,
+            approvals: 0
+        });
+        
+        transferRequests.push(transferRequest);
     }
     
+    function approve(uint _txId) public ownlyOwners selfApprovalRestriction(_txId) {
+        // Cannot vote twice
+        require(approvals[msg.sender][_txId] == false);
+        // Make sure transfer has not already been sent
+        require(transferRequests[_txId].hasBeenSent == false);
+        approvals[msg.sender][_txId] = true;
+        transferRequests[_txId].approvals++;
+        if(transferRequests[_txId].approvals >= limit) {
+            transferRequests[_txId].hasBeenSent = true;
+            // PAY
+            transferRequests[_txId].receiver.transfer(transferRequests[_txId].amount);
+        }
+    }
     
-     
+    function getTransferRequests() public view returns (Transfer[] memory) {
+        return transferRequests;
+    }
+    
 }
-
